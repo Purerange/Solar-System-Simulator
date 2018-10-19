092477a9964dabecc64e1101c794080bbf575441
dair68 <granthuang68@yahoo.com>
2018-10-19 01:39:00 -0700

    full explosion

--- assets/javascript/sun.js
+++ assets/javascript/sun.js
@@ -4,43 +4,47 @@ $(document).ready(function () {
     var planets = {
         sun: {
             name: "Sun",
-            radius: 10,
+            radius: 21,
             revolution: 9, //in earth days
             sunDistance: 0, //this is actually distance from baricenter
             orbitDirection: "clockwise",
             angle: 0,
             color: "yellow",
             img: "assets/images/sun.png",
+            show: true
         },
         mercury: {
             name: "Mercury",
             radius: 9,
             revolution: 88,
-            sunDistance: 97,
+            sunDistance: 130,
             orbitDirection: "clockwise",
             angle: 0,
             color: "lightgrey",
-            img: "assets/images/mercury.png"
+            img: "assets/images/mercury.png",
+            show: true
         },
         venus: {
             name: "Venus",
             radius: 12,
             revolution: 220,
-            sunDistance: 140,
+            sunDistance: 210,
             orbitDirection: "clockwise",
             angle: 0,
             color: "gold",
-            img: "assets/images/venus.png"
+            img: "assets/images/venus.png",
+            show: true
         },
         earth: {
             name: "Earth",
             radius: 12,
             revolution: 365,
-            sunDistance: 196,
+            sunDistance: 310,
             orbitDirection: "clockwise",
             angle: 0,
             color: "green",
             img: "assets/images/earth2.png",
+            show: true,
             satellites: {
                 moon: {
                     name: "Moon",
@@ -50,7 +54,8 @@ $(document).ready(function () {
                     orbitDirection: "clockwise",
                     angle: 2 * Math.PI,
                     color: "white",
-                    img: "assets/images/moon.png"
+                    img: "assets/images/moon.png",
+                    show: true
                 },
             }
         },
@@ -58,11 +63,12 @@ $(document).ready(function () {
             name: "Mars",
             radius: 10,
             revolution: 687,
-            sunDistance: 255,
+            sunDistance: 420,
             orbitDirection: "clockwise",
             angle: 0,
             color: "red",
-            img: "assets/images/mars.png"
+            img: "assets/images/mars.png",
+            show: true
         }
     }
 
@@ -107,6 +113,7 @@ $(document).ready(function () {
     var timescale = [-.05, 0, 4.57, 9.84, 11.6, 12.27, 12.27001, 12.37, 12.39, 12.3905];
     var solar_radius = [0, .93, 1, 1.75, 3.5, 180, 10, 20, 200, .03];
 
+    //this data is NOT to scale!
     var sunStates = [
         {
             startRadius: 1,
@@ -114,7 +121,7 @@ $(document).ready(function () {
             radius: 1,
             startTime: 4.57,
             endTime: 9.84,
-            timeIncrement: .001
+            intervals: 47
         },
         {
             startRadius: 1.75,
@@ -122,51 +129,51 @@ $(document).ready(function () {
             radius: 1.75,
             startTime: 9.84,
             endTime: 11.6,
-            timeIncrement: .001
-        }, 
+            intervals: 40
+        },
         {
             startRadius: 3.5,
-            endRadius: 180,
+            endRadius: 14,
             radius: 3.5,
             startTime: 11.6,
             endTime: 12.27,
-            timeIncrement: .001
+            intervals: 30
         },
         {
-            startRadius: 180,
-            endRadius: 10,
-            radius: 180,
+            startRadius: 14,
+            endRadius: 5,
+            radius: 14,
             startTime: 12.27,
             endTime: 12.27001,
-            timeIncrement: .001
-        }, 
-        {
-            startRadius: 10,
-            endRadius: 20,
-            radius: 10,
-            startTime: 12.27001,
-            endTime: 12.37,
-            timeIncrement: .001
+            intervals: 5
         },
         {
-            startRadius: 20,
-            endRadius: 200,
-            radius: 20,
+            startRadius: 5,
+            endRadius: 8,
+            radius: 5,
+            startTime: 12.27001,
+            endTime: 12.37,
+            intervals: 11
+        },
+        {
+            startRadius: 8,
+            endRadius: 15,
+            radius: 8,
             startTime: 12.37,
             endTime: 12.39,
-            timeIncrement: .001
-        }, 
+            intervals: 6
+        },
         {
-            startRadius: 200,
-            endRadius: .03,
-            radius: 200,
+            startRadius: 15,
+            endRadius: .5,
+            radius: 15,
             startTime: 12.39,
-            endTime: 12.3905,
-            timeIncrement: .001
+            endTime: 15,
+            intervals: 255
         }
     ];
 
-    
+
     //draws static elements of model including background color and orbit rings
     function drawBackground() {
         //creating black background
@@ -205,11 +212,11 @@ $(document).ready(function () {
         //clearing canvas
         ctx.clearRect(0, 0, width, height);
 
-         //animating sun explosion
-         if (explodingSunMode) {
-             //console.log("exploding");
-            $.each(sunStates, function(key, timeframe) {
-                console.log(planets.sun.radius);
+        //animating sun explosion
+        if (explodingSunMode) {
+            //console.log("exploding");
+            $.each(sunStates, function (key, timeframe) {
+                //console.log(planets.sun.radius);
 
                 var start = timeframe.startTime * timeNormalizer;
                 var end = timeframe.endTime * timeNormalizer;
@@ -219,30 +226,36 @@ $(document).ready(function () {
                 // console.log(year);
 
                 //figuring out which moment of time the sun is at
-                if(start <= year && year < end) {
+                if (start <= year && year < end) {
+                    console.log(year);
+                    //console.log(timeframe.radius);
+                    console.log(planets.sun.radius);
                     //finding number of years between start/end date
-                    var numInterals = end - start;
+                    //var numInterals = end - start;
 
                     //for scaling Sun's final radius so that it reaches earth's orbit in end
                     //var earthScaler = startingSunRadius * 
 
                     //how much the Sun's radius multiplier will change
-                    var radiusIncrease = (timeframe.endRadius - timeframe.startRadius) / numInterals;
+                    var radiusIncrease = (timeframe.endRadius - timeframe.startRadius) / timeframe.intervals;
+                    console.log(radiusIncrease);
 
                     //the new Sun radius multiplier, a number indicating size relative to when explode button first clicked
-                    timeframe.radius += radiusIncrease; 
+                    timeframe.radius += radiusIncrease;
 
                     //changing Sun's radius
                     planets.sun.radius = startingSunRadius * timeframe.radius;
-                    year+=timeframe.timeIncrement * timeNormalizer;
-
-                    console.log(year);
+                    year += (end - start) / timeframe.intervals;
                 }
             });
         }
 
         //drawing all the planets for this particular instance of time
         $.each(spaceBodies, function (key, planet) {
+            if (planets.sun.radius >= planet.sunDistance && planet.name !== "Sun") {
+                planet.show = false;
+            }
+
             //drawing planet
             var sunDist = planet.sunDistance;
             var x = originX + sunDist * Math.cos(planet.angle);
@@ -252,7 +265,7 @@ $(document).ready(function () {
             var planetY = y - planet.radius;
 
             //planet has an image representation
-            if (planet.img !== null) {
+            if (planet.img !== null && planet.show === true) {
                 //console.log(planet.name);
                 var img = new Image();
                 img.src = planet.img;
@@ -272,6 +285,10 @@ $(document).ready(function () {
             if (planet.hasOwnProperty("satellites")) {
                 //console.log(planet.satellites);
                 $.each(planet.satellites, function (key, moon) {
+                    if (planets.earth.show === false) {
+                        moon.show = false;
+                    }
+
                     //console.log("drawing moon");
                     //drawing satellite
                     var satelliteX = x + moon.planetDistance * Math.cos(moon.angle);
@@ -280,10 +297,12 @@ $(document).ready(function () {
                     moonX = satelliteX - moon.radius;
                     moonY = satelliteY - moon.radius;
 
-                    var img = new Image();
-                    img.src = moon.img;
+                    if (moon.show == true) {
+                        var img = new Image();
+                        img.src = moon.img;
 
-                    ctx.drawImage(img, moonX, moonY, moon.radius * 2, moon.radius * 2);
+                        ctx.drawImage(img, moonX, moonY, moon.radius * 2, moon.radius * 2);
+                    }
 
                     //at normal time speed, 1 earth day = 1 frame
                     var orbitRate = 2 * Math.PI / moon.revolution;
@@ -302,7 +321,7 @@ $(document).ready(function () {
                 });
             }
 
-           
+
         });
     }
 
@@ -389,13 +408,13 @@ $(document).ready(function () {
     var explodingSunMode = false;
     //var sunIterator = 
     var explosionSpeed = 1;
-    
+
     var startingSunRadius = planets.sun.radius;
     var radiusMultiplier = 1;
 
     //used to ensure all time values for sun explosion are integers
-    var timeNormalizer = 100000;
-    var year = 4.57*timeNormalizer;
+    var timeNormalizer = 100000000000;
+    var year = 4.57 * timeNormalizer;
 
     var paused = false;
     var spaceBodies = planets;
